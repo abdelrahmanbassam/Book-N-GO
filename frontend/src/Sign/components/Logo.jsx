83799fbfb30173d28adcd97739fa49d98760d574
Bassam <abdelrahmanbassam0109@gmail.com>
2024-12-03 21:47:33 +0200

    add logo from figma

--- frontend/src/Sign/components/Logo.jsx
+++ frontend/src/Sign/components/Logo.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BookIcon } from 'lucide-react';
 import styles from './Logo.module.css';
+import logoImage from '../../assets/Logo.png'; // Adjust the path as needed
 
 export const Logo = () => (
-  <div className={styles.logo}>
-    <BookIcon size={24} color="#FF9944" />
-    <span className={styles.logoText}>Book'nGo</span>
+  <div className={styles.Logo}>
+    <img src={logoImage} alt="Logo" className={styles.logoImage} />
+    {/* <span className={styles.logoText}>Book'nGo</span> */}
   </div>
 );
