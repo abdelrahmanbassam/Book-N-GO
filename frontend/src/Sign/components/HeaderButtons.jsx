974b6c6329f88b2f868e8295e72deb744d3928cb
Bassam <abdelrahmanbassam0109@gmail.com>
2024-12-03 18:19:27 +0200

    fix header buttons

--- frontend/src/Sign/components/HeaderButtons.jsx
+++ frontend/src/Sign/components/HeaderButtons.jsx
@@ -6,6 +6,11 @@ export const HeaderButtons = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const isLoginPage = location.pathname === '/login';
+  const isSignUpPage = location.pathname === '/signup';
+
+  // console.log('Current Path:', location.pathname);
+  // console.log('Is Login Page:', isLoginPage);
+  // console.log('Is Sign Up Page:', isSignUpPage);
 
   return (
     <div className={styles.headerButtons}>
@@ -19,7 +24,7 @@ export const HeaderButtons = () => {
       <button 
         className={`${styles.headerButton} ${styles.signUpBtn}`}
         onClick={() => navigate('/signup')}
-        disabled={!isLoginPage}
+        disabled={isSignUpPage}
       >
         SIGN UP
       </button>
