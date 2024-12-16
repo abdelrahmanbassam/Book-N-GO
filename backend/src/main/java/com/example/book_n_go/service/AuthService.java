7ddab23cc48c2e435d50fee4a932423b79aaf292
Ahmad Hassan <mi010203269@gmail.com>
2024-12-16 22:47:58 +0200

    add user request retreiver

--- backend/src/main/java/com/example/book_n_go/service/AuthService.java
+++ backend/src/main/java/com/example/book_n_go/service/AuthService.java
@@ -4,6 +4,8 @@ import java.util.NoSuchElementException;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -53,5 +55,10 @@ public class AuthService {
         } catch (NoSuchElementException e) {
             throw new Exception("Invalid credentials", e);
         }
+    }
+
+    public static User getRequestUser(){
+      return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      
     }
 }
