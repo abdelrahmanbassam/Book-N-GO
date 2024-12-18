6d8f719b0b781201e24c18983c9d1b8938605097
MouradMahgoub <mouradmahgoub710@gmail.com>
2024-12-18 03:19:45 +0200

    refactor: update SecurityConfig to use CORS configuration; modify BookingController and HallController for improved endpoint structure and data retrieval

--- backend/src/main/java/com/example/book_n_go/config/SecurityConfig.java
+++ backend/src/main/java/com/example/book_n_go/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.book_n_go.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.example.book_n_go.securityFilter.JwtAuthenticationFilter;
 
@@ -24,6 +26,8 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,7 +36,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors().configurationSource(corsConfigurationSource).and()
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/signup", "/auth/login").permitAll()
