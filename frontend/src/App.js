c0ffd9cbf3d2bf3c46b2a2fe0f5fd1ea9ad9e852
Ahmad Hassan <mi010203269@gmail.com>
2024-12-30 23:48:25 +0200

    customer service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,28 +9,38 @@ import { HallDetails } from './HallDetails/HallDetails';
 import { Login } from './Sign/LoginPage/Login';
 import { SignUp } from './Sign/SignUpPage/SignUp';
 
-import {WorkSpace} from "./WorkSpace/WorkSpace";
-import { Navigate } from 'react-router-dom';
+import { WorkSpace } from "./WorkSpace/WorkSpace";
 
 import { HallsList } from './HallsList&Filter/HallsListPage/HallsList';
 import { SelectRole } from './Sign/SelectRolePage/SelectRole';
+import { PageContext } from './PageContext';
+import { useRef } from "react";
+import Copilot from "./components/Copilot";
+
 
 function App() {
+  const divRef = useRef(null); // Create a reference to the div
+
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
-        {/* <Route path='/' element={<HomePage/>}/> */}
-        { <Route path='/workspace' element={<WorkSpace/>}/> }
-        { <Route path='/workspace/:workspaceId' element={<WorkSpace/>}/> }
-        {/* {<Route path='/hall' element={<HallDetails/>}/>} */}
-        {<Route path='/login' element={<Login/>}/>}
-        {<Route path='/signup' element={<SignUp/>}/>}
-        <Route path='/workspace/:workspaceId/hall/:id' element={<HallDetails/>}/>
-        {<Route path="/select-role" element={<SelectRole />} />}
-        {/* <Route path='/login' element={<LoginPage/>}/> */}
-        <Route path='/hallsList' element={<HallsList/>}/>
-      </Routes>
+      <div className="App" ref={divRef}>
+        <PageContext.Provider value={{ divRef }}>
+          <Routes>
+            <Route path='/' element={<Navigate to="/login" />} />
+            {/* <Route path='/' element={<HomePage/>}/> */}
+            {<Route path='/workspace' element={<WorkSpace />} />}
+            {<Route path='/workspace/:workspaceId' element={<WorkSpace />} />}
+            {/* {<Route path='/hall' element={<HallDetails/>}/>} */}
+            {<Route path='/login' element={<Login />} />}
+            {<Route path='/signup' element={<SignUp />} />}
+            <Route path='/workspace/:workspaceId/hall/:id' element={<HallDetails />} />
+            {<Route path="/select-role" element={<SelectRole />} />}
+            {/* <Route path='/login' element={<LoginPage/>}/> */}
+            <Route path='/hallsList' element={<HallsList />} />
+          </Routes>
+          <Copilot/>
+        </PageContext.Provider>
+      </div>
     </Router>
   );
 }
