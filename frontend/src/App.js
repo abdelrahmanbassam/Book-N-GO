0cdc0dcdaf754e153b2e3ff61c86cf3c291f64f6
AbdElRahman Osama <bodyusama207@gmail.com>
2024-12-30 22:07:19 +0200

    removing duplicate Navigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,43 @@
 import {
-  Navigate,
-  Route,
-  BrowserRouter as Router,
-  Routes,
+    Navigate,
+    Route,
+    BrowserRouter as Router,
+    Routes,
 } from "react-router-dom";
-import './App.css';
-import { HallDetails } from './HallDetails/HallDetails';
-import { Login } from './Sign/LoginPage/Login';
-import { SignUp } from './Sign/SignUpPage/SignUp';
-
-import {WorkSpace} from "./WorkSpace/WorkSpace";
-import { Navigate } from 'react-router-dom';
-
-import { HallsList } from './HallsList&Filter/HallsListPage/HallsList';
-import { SelectRole } from './Sign/SelectRolePage/SelectRole';
+import "./App.css";
+import { HallDetails } from "./HallDetails/HallDetails";
+import { Login } from "./Sign/LoginPage/Login";
+import { SignUp } from "./Sign/SignUpPage/SignUp";
+import { WorkSpace } from "./WorkSpace/WorkSpace";
+import { HallsList } from "./HallsList&Filter/HallsListPage/HallsList";
+import { SelectRole } from "./Sign/SelectRolePage/SelectRole";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
-        {/* <Route path='/' element={<HomePage/>}/> */}
-        { <Route path='/workspace' element={<WorkSpace/>}/> }
-        { <Route path='/workspace/:workspaceId' element={<WorkSpace/>}/> }
-        {/* {<Route path='/hall' element={<HallDetails/>}/>} */}
-        {<Route path='/login' element={<Login/>}/>}
-        {<Route path='/signup' element={<SignUp/>}/>}
-        <Route path='/workspace/:workspaceId/hall/:id' element={<HallDetails/>}/>
-        {<Route path="/select-role" element={<SelectRole />} />}
-        {/* <Route path='/login' element={<LoginPage/>}/> */}
-        <Route path='/hallsList' element={<HallsList/>}/>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />
+                {/* <Route path='/' element={<HomePage/>}/> */}
+                {<Route path="/workspace" element={<WorkSpace />} />}
+                {
+                    <Route
+                        path="/workspace/:workspaceId"
+                        element={<WorkSpace />}
+                    />
+                }
+                {/* {<Route path='/hall' element={<HallDetails/>}/>} */}
+                {<Route path="/login" element={<Login />} />}
+                {<Route path="/signup" element={<SignUp />} />}
+                <Route
+                    path="/workspace/:workspaceId/hall/:id"
+                    element={<HallDetails />}
+                />
+                {<Route path="/select-role" element={<SelectRole />} />}
+                {/* <Route path='/login' element={<LoginPage/>}/> */}
+                <Route path="/hallsList" element={<HallsList />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
